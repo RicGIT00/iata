fff361ccf90a414b8199b5d86bd22df5a72ab87b
eduardochiaratto <eduardochiaratto23@gmail.com>
2019-10-08 03:25:16 -0300

    Criando botão de sair

--- js/jobs-rh.js
+++ js/jobs-rh.js
@@ -1,3 +1,7 @@
 document.getElementById("card-button").addEventListener("click",function(){
   location.href='view-job.html';
 });
+
+document.getElementById("card").addEventListener("click",function(){
+  location.href='applicants-view.html';
+});
