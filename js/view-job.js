7bbfae74a331dee6f9e86fb12fb78da3c36f80f5
eduardochiaratto <eduardochiaratto23@gmail.com>
2019-10-08 03:02:46 -0300

    Criando tabela de edit/delete de vagas

---
+++ js/view-job.js
@@ -0,0 +1,7 @@
+document.getElementById("job-edit").addEventListener("click",function(){
+  location.href='jobs-rh.html';
+});
+
+document.getElementById("delete-button").addEventListener("click",function(){
+  location.href='jobs-rh.html';
+});
